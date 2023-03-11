52eb01fb458520cc2c4d17f10aad98d9e1c31542
Rahulcen <rahulcen.er@gmail.com>
2023-03-11 11:16:24 +0530

    first week code commited

--- oopsday4/src/oopsday4/polymorphism/Shape.java
+++ oopsday4/src/oopsday4/polymorphism/Shape.java
@@ -2,4 +2,9 @@ package oopsday4.polymorphism;
 
 public class Shape {
 
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- oopsday4/src/oopsday4/polymorphism/TestShapes.java
+++ oopsday4/src/oopsday4/polymorphism/TestShapes.java
@@ -1,16 +1,24 @@
-//package oopsday4.polymorphism;
-//public class TestShapes {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		Shape s=new Rectangle(10,25,65,75);
-//	    double result=s.area();
-//	    System.out.println(result);
-//	    
-//	    s=new circle(65.87);
-//	    result= s.area();
-//	    
-//
-//	}
-//
-//}
+package oopsday4.polymorphism;
+
+public class TestShapes {
+
+	public static void main(String[] args) {
+		
+		Shape s = new Rectangle();
+		double result = s.area();
+		System.out.println(result);
+		
+		s = new Circle(65.87);
+		result = s.area();
+		System.out.println(result);
+		
+		s = new Square(55);
+		result = s.area();
+		System.out.println(result);
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
